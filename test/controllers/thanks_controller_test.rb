94130f403d2b9b5329963b2a8d35e4a0c8394df6
朴晋豪 <pakchino@pch.local>
2026-01-14 13:39:39 +0900

    Issue #7: ありがとう記録の新規作成機能完了

---
+++ test/controllers/thanks_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class ThanksControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get thanks_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get thanks_create_url
+    assert_response :success
+  end
+end
