0519c2d32321b7f7ec3c7b5d5c2370550ece0ce4
朴晋豪 <pakchino@pch.local>
2026-01-15 21:09:46 +0900

    テスト修正:fixtureを削除、Deviseヘルパー追加、テストコード修正

--- test/controllers/thanks_controller_test.rb
+++ test/controllers/thanks_controller_test.rb
@@ -1,13 +1,27 @@
 require "test_helper"
 
 class ThanksControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = User.create!(email: "test@example.com", password: "password")
+    sign_in @user
+  end
+
   test "should get new" do
-    get thanks_new_url
+    get new_thank_path
     assert_response :success
   end
 
-  test "should get create" do
-    get thanks_create_url
-    assert_response :success
+  test "should create thank" do
+    assert_difference("Thank.count", 1) do
+      post thanks_path, params: {
+        thank: {
+          date: Date.today,
+          from_who: "テストユーザー",
+          situation: "テスト状況",
+          feeling: "テスト感想"
+        }
+      }
+    end
+    assert_redirected_to root_path
   end
 end
