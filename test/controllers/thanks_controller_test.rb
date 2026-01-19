fe0d3e8665c7a5ef1fca778a624324ab4954163d
朴晋豪 <pakchino@pch.local>
2026-01-19 18:11:52 +0900

    ありがとう記録の編集・削除機能を追加

--- test/controllers/thanks_controller_test.rb
+++ test/controllers/thanks_controller_test.rb
@@ -62,4 +62,61 @@ class ThanksControllerTest < ActionDispatch::IntegrationTest
     get thank_url(other_thank)
     assert_response :not_found
   end
+
+  test "should get edit when logged in" do
+    sign_in @user
+    get edit_thank_url(@thank)
+    assert_response :success
+  end
+
+  test "should redirect to login when not logged in for edit" do
+    get edit_thank_url(@thank)
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should update thank when logged in" do
+    sign_in @user
+    patch thank_url(@thank), params: { thank: {
+      from_who: "更新後の名前"
+    } }
+    assert_redirected_to thank_url(@thank)
+    @thank.reload
+    assert_equal "更新後の名前", @thank.from_who
+  end
+
+  test "should not update other user's thank" do
+    other_user = User.create!(email: "other@example.com", password: "password")
+    other_thank = other_user.thanks.create!(
+      date: Date.today,
+      from_who: "他のユーザー",
+      situation: "他のユーザーの記録"
+    )
+
+    sign_in @user
+    patch thank_url(other_thank), params: { thank: { from_who: "変更" } }
+    assert_response :not_found
+  end
+
+  test "should destroy thank when logged in" do
+    sign_in @user
+    assert_difference("Thank.count", -1) do
+      delete thank_url(@thank)
+    end
+    assert_redirected_to thanks_path
+  end
+
+  test "should not destroy other user's thank" do
+    other_user = User.create!(email: "other@example.com", password: "password")
+    other_thank = other_user.thanks.create!(
+      date: Date.today,
+      from_who: "他のユーザー",
+      situation: "他のユーザーの記録"
+    )
+
+    sign_in @user
+    assert_no_difference("Thank.count") do
+      delete thank_url(other_thank)
+    end
+    assert_response :not_found
+  end
 end
