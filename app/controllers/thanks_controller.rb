8e4a0269a591cda64c5d5d9add8379ca75879276
朴晋豪 <pakchino@pch.local>
2026-01-19 17:57:10 +0900

    Add show action for thanks

--- app/controllers/thanks_controller.rb
+++ app/controllers/thanks_controller.rb
@@ -1,5 +1,6 @@
 class ThanksController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_thank, only: [ :show ]  # 追加
 
   def index
     @thanks = current_user.thanks.order(date: :desc)
@@ -12,13 +13,22 @@ class ThanksController < ApplicationController
   def create
     @thank = current_user.thanks.build(thank_params)
     if @thank.save
-      redirect_to root_path, notice: "ありがとう記録を保存しました"
+      redirect_to thanks_path, notice: "ありがとう記録を作成しました"
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  # 追加
+  def show
+  end
+
   private
+
+  # 追加
+  def set_thank
+    @thank = current_user.thanks.find(params[:id])
+  end
 
   def thank_params
     params.require(:thank).permit(:date, :from_who, :situation, :feeling)
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root "static_pages#top"
 
-  resources :thanks, only: [ :index, :new, :create ]
+  resources :thanks, only: [ :index, :new, :create, :show ]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
