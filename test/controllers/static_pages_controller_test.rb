ec01d8ff4173b8c28750bd9eba37c159db31d8d3
朴晋豪 <pakchino@pch.local>
2026-01-20 20:49:59 +0900

    トップページを改修(未ログイン時説明・ログイン時ランダム表示)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,4 +5,47 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get root_url
     assert_response :success
   end
+
+  test "should show description when not logged in" do
+    get root_url
+    assert_response :success
+    assert_match "ありがとう貯金とは", @response.body
+    assert_match "新規登録", @response.body
+    assert_match "ログイン", @response.body
+  end
+
+  test "should show random thank when logged in and has records" do
+    user = User.create!(email: "test@example.com", password: "password")
+    thank = user.thanks.create!(
+      date: Date.today,
+      from_who: "テストユーザー",
+      situation: "テスト状況"
+    )
+
+    sign_in user
+    get root_url
+    assert_response :success
+    assert_match "今日のありがとう", @response.body
+    assert_match thank.from_who, @response.body
+  end
+
+  test "should show no records message when logged in but no records" do
+    user = User.create!(email: "test@example.com", password: "password")
+
+    sign_in user
+    get root_url
+    assert_response :success
+    assert_match "まだありがとう記録がありません", @response.body
+  end
+
+  test "should show action buttons when logged in" do
+    user = User.create!(email: "test@example.com", password: "password")
+
+    sign_in user
+    get root_url
+    assert_response :success
+    assert_match "記録を見る", @response.body
+    assert_match "新規作成", @response.body
+    assert_match "ログアウト", @response.body
+  end
 end
