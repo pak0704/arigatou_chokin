1355a7b2a630223802c8bcd3b243b2761bd716e4
朴晋豪 <pakchino@pch.local>
2026-01-25 00:25:25 +0900

    Kaminariを導入してページネーション機能を追加

--- app/controllers/thanks_controller.rb
+++ app/controllers/thanks_controller.rb
@@ -21,6 +21,8 @@ class ThanksController < ApplicationController
     if params[:situation].present?
       @thanks = @thanks.where("situation LIKE ?", "%#{params[:situation]}%")
     end
+
+      @thanks = @thanks.page(params[:page]).per(10)
   end
 
   def new
